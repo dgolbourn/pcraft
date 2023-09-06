316f8dda0d7ad09de973d7e508d94c724fa4d65d
Diane Marigold <golbourn@gmail.com>
2023-09-06 16:03:33 +0100

    mc_init

--- mc_init/start.sh
+++ mc_init/start.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
+mkdir -p /output/data
 mc-image-helper vanillatweaks \
-    --output-directory=/data \
+    --output-directory=/output/data \
     --world-subdir=world \
     --share-codes="" \
     --pack-files="$VANILLATWEAKS_FILE"
 
 mc-image-helper modrinth \
-      --output-directory=/data \
+      --output-directory=/output/data \
       --projects="$MODRINTH_PROJECTS" \
       --game-version="$VERSION" \
       --loader="$TYPE" \
       --download-optional-dependencies="false" \
       --allowed-version-type="release"
 
-OUTPUT=$(sha1sum /data/resourcepacks/*)
+cd /output/data
+OUTPUT=$(sha1sum resourcepacks/*)
 OUTPUTS=($OUTPUT)
-mkdir -p /web/data
-cp -r /data/resourcepacks /web/data
-cp -r /data/mods /web/data
 echo "RESOURCE_PACK_SHA1=${OUTPUTS[0]}" > /output/.env
-echo "RESOURCE_PACK=${HOST-http://localhost}:8080${OUTPUTS[1]}" >> /output/.env
+echo "RESOURCE_PACK=${HOST-http://localhost}:8080/${OUTPUTS[1]}" >> /output/.env
