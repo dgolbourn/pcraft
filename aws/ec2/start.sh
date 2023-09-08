53a571921ff7ab7265e2b7c8fff42797c0f13d8e
Diane Golbourn <golbourn@gmail.com>
2023-09-08 03:49:54 +0100

    absolute paths

--- aws/ec2/start.sh
+++ aws/ec2/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker-compose -f /percycraft/docker-compose.mc_init.yml up
-docker-compose -f /percycraft/docker-compose.mc.yml up
+docker-compose -f /percycraft/docker-compose.mc.yml --env-file /percycraft/.env up
