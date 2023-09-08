f358f7df2fde6f546533817fee7ebc2b3efc4113
Diane Marigold <golbourn@gmail.com>
2023-09-08 20:36:52 +0100

    Update install.sh

--- aws/ec2/install.sh
+++ aws/ec2/install.sh
@@ -3,7 +3,7 @@ yum update -y
 yum install -y amazon-efs-utils
 mkdir /efs
 mount -t efs ${1}:/ /efs
-sh -c "yes | amazon-linux-extras install docker"
+yes | amazon-linux-extras install docker
 yum erase amazon-ssm-agent -y
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
