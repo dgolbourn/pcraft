48a8acca00f911d54e873ad3b647b16de23775e5
Diane Marigold <golbourn@gmail.com>
2023-09-10 11:21:12 +0100

    typo

--- mc_init/init.sh
+++ mc_init/init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ "$(ls -A /data)" ] && [ "$(ls -A /data/mods)" ] && [ "$(ls -A /data/resourcepakcs)" ] && [ "$(ls -A /data/world/datapacks)" ] && [ "$(ls -A /data/*.jar)" ] && echo "data found" || /start
+[ "$(ls -A /data)" ] && [ "$(ls -A /data/mods)" ] && [ "$(ls -A /data/resourcepacks)" ] && [ "$(ls -A /data/world/datapacks)" ] && [ "$(ls -A /data/*.jar)" ] && echo "data found" || /start
