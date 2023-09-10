7e7e5f6c6a0f2d943ef0f57b809263807e59a47b
Diane Marigold <golbourn@gmail.com>
2023-09-10 11:26:00 +0100

    start normally

--- mc_init/init.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-[ "$(ls -A /data)" ] && [ "$(ls -A /data/mods)" ] && [ "$(ls -A /data/resourcepacks)" ] && [ "$(ls -A /data/world/datapacks)" ] && [ "$(ls -A /data/*.jar)" ] && echo "data found" || /start
