3c52560c6c44da88eb047862aa0459151c64afda
Diane Marigold <golbourn@gmail.com>
2023-09-11 11:29:25 +0100

    ssm back

--- aws/ec2/install.sh
+++ aws/ec2/install.sh
@@ -4,7 +4,6 @@ yum install -y amazon-efs-utils
 mkdir /efs
 mount -t efs ${1}:/ /efs
 yes | amazon-linux-extras install docker
-yum erase amazon-ssm-agent -y
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 curl -fsSL -o mcaselector.jar https://github.com/Querz/mcaselector/releases/download/2.2.2/mcaselector-2.2.2.jar        
