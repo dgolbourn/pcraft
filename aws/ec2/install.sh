d1bc7b9ff8a5f80c73950e2022bb9a19398e4b9e
Diane Golbourn <golbourn@gmail.com>
2023-09-14 19:32:55 +0100

    put data and web on ebs

--- aws/ec2/install.sh
+++ aws/ec2/install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-yum update -y
-yum install -y amazon-efs-utils
-mkdir /efs
-mount -t efs ${1}:/ /efs
 yum install -y docker
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -11,11 +7,11 @@ tar -xvzf mcrcon.tar.gz
 mv mcrcon /usr/local/bin/
 rm mcrcon.tar.gz
 chmod +x /usr/local/bin/mcrcon
-mkdir -p /efs/data
+mkdir -p /opt/data
 mkdir -p /efs/backups
-mkdir -p /efs/web
-chmod +x /percycraft/mc_init/pre-start.sh
-cp /percycraft/aws/ec2/percycraft.service /etc/systemd/system/percycraft.service
+mkdir -p /opt/web
+chmod +x /opt/percycraft/mc_init/pre-start.sh
+cp /opt/percycraft/aws/ec2/percycraft.service /etc/systemd/system/percycraft.service
 systemctl enable docker.service
 systemctl enable percycraft.service
 systemctl start docker.service
