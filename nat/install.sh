9b46e387968e019066629d62e605e5638082176e
Diane Golbourn <golbourn@gmail.com>
2023-09-19 16:59:07 +0100

    install

--- nat/install.sh
+++ nat/install.sh
@@ -3,3 +3,5 @@ mkdir -p /opt/lazymc
 sudo curl -fsSL -o /opt/lazymc/lazymc https://github.com/timvisee/lazymc/releases/download/v0.2.10/lazymc-v0.2.10-linux-aarch64
 chmod +x /opt/lazymc/lazymc
 chmod +x /opt/percycraft/nat/server.sh
+cp /opt/percycraft/nat/server.sh /opt/lazymc
+cp /opt/percycraft/nat/lazymc.toml /opt/lazymc
