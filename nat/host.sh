f6e35da54608b716ef1b2ccac29a81b416cc5de5
Diane Golbourn <golbourn@gmail.com>
2023-09-20 19:06:11 +0100

    use another elastic ip

--- nat/host.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i "s/\([^\t ]*\)\([\t ]*\)percycraft/$1\2percycraft/g" /etc/hosts
--- nat/install.sh
+++ nat/install.sh
@@ -3,7 +3,8 @@ mkdir -p /opt/lazymc
 sudo curl -fsSL -o /opt/lazymc/lazymc https://github.com/timvisee/lazymc/releases/download/v0.2.10/lazymc-v0.2.10-linux-aarch64
 chmod +x /opt/lazymc/lazymc
 chmod +x /opt/percycraft/nat/server.sh
-chmod +x /opt/percycraft/nat/host.sh
 cp /opt/percycraft/nat/server.sh /opt/lazymc
 cp /opt/percycraft/nat/lazymc.toml /opt/lazymc
-echo "x.x.x.x percycraft" >> /etc/hosts
+cp /opt/percycraft/nat/lobby.service /etc/systemd/system/lobby.service            
+systemctl enable lobby.service
+systemctl start lobby.service
