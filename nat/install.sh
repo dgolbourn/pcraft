e2b1c5353cbf28d5811e13b742da125e17dcbdf7
Diane Golbourn <golbourn@gmail.com>
2023-09-20 20:16:38 +0100

    working udp

--- nat/install.sh
+++ nat/install.sh
@@ -2,9 +2,9 @@
 yum install -y iptables-services
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p 
-iptables -t nat -A POSTROUTING -j MASQUERADE 
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -t nat -A PREROUTING -p udp --dport 24454 -j DNAT --to-destination 18.169.42.16:24454 
 service iptables save 
-
 mkdir -p /opt/lazymc
 sudo curl -fsSL -o /opt/lazymc/lazymc https://github.com/timvisee/lazymc/releases/download/v0.2.10/lazymc-v0.2.10-linux-aarch64
 chmod +x /opt/lazymc/lazymc
