f77a0d8aff1aa40568115e04448b69c2809e6773
Diane Marigold <golbourn@gmail.com>
2023-09-22 09:18:04 +0100

    Revert "what"
    This reverts commit 2a218a468672aee69a8e6713d3b19f31d4bc8f5e.

--- aws/ec2/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Backup started"
-ts=$(date +"%Y%m%d-%H%M%S")
-outFile="/efs/backups/world-${ts}.tgz"
-tar --gzip -cf "${outFile}" -C /opt/data .
-ln -sf ${outFile} /efs/backups/latest.tgz
-find /efs/backups -maxdepth 1 -name *.tgz -mtime +7 "${@}" -delete
-echo "Backup complete"
