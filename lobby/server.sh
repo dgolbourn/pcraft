c796ac1d8650dc59cc53bc186dc3c615dc3a28cd
Diane Marigold <golbourn@gmail.com>
2023-09-23 16:27:58 +0100

    Update server.sh

--- lobby/server.sh
+++ lobby/server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 startup() {
     echo "Server started"
+    iptables -D INPUT -s $IP -j DROP
     aws lambda invoke --function-name percycraft-StartLambda /dev/null
     while true; do
         sleep 1
@@ -9,11 +10,13 @@ startup() {
 
 shutdown() {
     kill $PID
+    iptables -A INPUT -s $IP -j DROP
     aws lambda invoke --function-name percycraft-StopLambda /dev/null
     echo "Server complete"
 }
 
 trap shutdown TERM INT
+IP=$1
 startup &
 PID=$!
 wait $PID
