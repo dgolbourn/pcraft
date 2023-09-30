317a567c934e761427ce7d7f7222cba1d3389a2c
Diane Marigold <golbourn@gmail.com>
2023-09-30 11:55:31 +0100

    typo

--- lobby/server.sh
+++ lobby/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 startup() {
     echo "Server started"
-    aws lambda invoke --function-name percycraft-StartStopLambda --payload "{\"start\":true,\"referrer\":\"server\"}" --cli-binary-format raw-in-base64-out/dev/null
+    aws lambda invoke --function-name percycraft-StartStopLambda --payload "{\"start\":true,\"referrer\":\"server\"}" --cli-binary-format raw-in-base64-out /dev/null
     nc -lv 25566
 }
 
