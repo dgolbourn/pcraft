c881fa05c9c31734c3631f2b0796a6b708c07337
Diane Marigold <golbourn@gmail.com>
2023-09-30 23:55:28 +0100

    tidy up

--- aws/ec2/image.sh
+++ aws/ec2/image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Generating png of world started"
-ts=$(date +"%Y%m%d-%H%M%S")
+source /opt/percycraft/service.env
+ts=$(date +"%Y%m%d")
 imageFile="/efs/album/world-${ts}.png"
 /opt/mca-selector/bin/java -D/opt/mca-selector/bin -Xmx6g -jar /opt/mca-selector/lib/mca-selector.jar --mode select --world /opt/data/world --query "InhabitedTime > \"2 minutes\"" --radius 5 --output selection.csv
 xvfb-run /opt/mca-selector/bin/java -D/opt/mca-selector/bin -Xmx6g -jar /opt/mca-selector/lib/mca-selector.jar --mode image --world /opt/data/world --selection selection.csv --output $imageFile
