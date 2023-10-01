c9ea44bef2bc8b80abf6331b0cd0800b23904343
Diane Golbourn <golbourn@gmail.com>
2023-10-01 15:51:33 +0100

    last 15 backups

--- aws/ec2/backup.sh
+++ aws/ec2/backup.sh
@@ -4,5 +4,5 @@ ts=$(date +"%Y%m%d-%H%M%S")
 outFile="/efs/backups/world-${ts}.tgz"
 tar --gzip -cf "${outFile}" -C /opt/data .
 ln -sf ${outFile} /efs/backups/latest.tgz
-find /efs/backups -maxdepth 1 -name *.tgz -mtime +7 "${@}" -delete
+find . -maxdepth 1 -type f -printf '%T@ %p\0' | sort -r -z -n | awk 'BEGIN { RS="\0"; ORS="\0"; FS="" } NR > 15 { sub("^[0-9]*(.[0-9]*)? ", ""); print }' | xargs -0 rm -f
 echo "Backup complete"
