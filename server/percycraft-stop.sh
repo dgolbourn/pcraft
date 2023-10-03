319ac99d1b78c3ec2128ec1a51d58d59ccfa43d8
Diane Marigold <golbourn@gmail.com>
2023-10-03 09:53:45 +0100

    backups

--- server/percycraft-stop.sh
+++ server/percycraft-stop.sh
@@ -12,6 +12,7 @@ backup() {
     outFile="/efs/backups/world-${ts}.tgz"
     tar --gzip -cf "${outFile}" -C /opt/data .
     ln -sf ${outFile} /efs/backups/latest.tgz
+    cd /efs/backups
     find . -maxdepth 1 -type f -printf '%T@ %p\0' | sort -r -z -n | awk 'BEGIN { RS="\0"; ORS="\0"; FS="" } NR > 8 { sub("^[0-9]*(.[0-9]*)? ", ""); print }' | xargs -0 rm -f
     echo Backup complete >&2
 }
