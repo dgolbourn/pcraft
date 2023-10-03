961388d29566b41954068a55b10d9c0653364a30
Diane Marigold <golbourn@gmail.com>
2023-10-03 16:39:37 +0100

    send album to deep archive bucket

--- server/percycraft-stop.sh
+++ server/percycraft-stop.sh
@@ -26,7 +26,7 @@ backup-efs() {
     echo Backup complete >&2
 }
 
-generate-image() {
+generate-image-efs() {
     echo Generating png of world started >&2
     ts=$(date +"%Y%m%d")
     imageFile="/efs/album/world-${ts}.png"
@@ -38,7 +38,19 @@ generate-image() {
     echo Generating png of world complete >&2
 }
 
+generate-image() {
+    echo Generating png of world started >&2
+    imageFile=/tmp/world.png
+    /opt/mca-selector/bin/java -D/opt/mca-selector/bin -Xmx6g -jar /opt/mca-selector/lib/mca-selector.jar --mode select --world /opt/data/world --query "InhabitedTime > \"2 minutes\"" --radius 5 --output selection.csv
+    xvfb-run /opt/mca-selector/bin/java -D/opt/mca-selector/bin -Xmx6g -jar /opt/mca-selector/lib/mca-selector.jar --mode image --world /opt/data/world --selection selection.csv --output $imageFile
+    rm selection.csv
+    aws s3 cp /tmp/world.png $ALBUMBUCKETS3URI
+    aws s3 cp /tmp/world.png $FILEBUCKETS3URI/album/
+    echo Generating png of world complete >&2
+}
+
 stop-server
 backup
-backup-efs
 generate-image
+backup-efs
+generate-image-efs
