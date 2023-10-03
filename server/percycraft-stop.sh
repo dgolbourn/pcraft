fa7166fe65c603675a1d652eb3d89d4f54b74a63
Diane Marigold <golbourn@gmail.com>
2023-10-03 18:45:24 +0100

    remove efs stage 2

--- server/percycraft-stop.sh
+++ server/percycraft-stop.sh
@@ -11,18 +11,19 @@ backup() {
     echo Backup started >&2
     tar --gzip -cf /tmp/data.tgz -C /opt/data .
     aws s3 cp /tmp/data.tgz $DATABUCKETS3URI
-    rm -rf /tmp/data.tgz
+    rm /tmp/data.tgz
     echo Backup complete >&2
 }
 
 generate-image() {
     echo Generating png of world started >&2
     imageFile=/tmp/world.png
-    /opt/mca-selector/bin/java -D/opt/mca-selector/bin -Xmx6g -jar /opt/mca-selector/lib/mca-selector.jar --mode select --world /opt/data/world --query "InhabitedTime > \"2 minutes\"" --radius 5 --output selection.csv
-    xvfb-run /opt/mca-selector/bin/java -D/opt/mca-selector/bin -Xmx6g -jar /opt/mca-selector/lib/mca-selector.jar --mode image --world /opt/data/world --selection selection.csv --output $imageFile
-    rm selection.csv
+    /opt/mca-selector/bin/java -D/opt/mca-selector/bin -Xmx6g -jar /opt/mca-selector/lib/mca-selector.jar --mode select --world /opt/data/world --query "InhabitedTime > \"2 minutes\"" --radius 5 --output /tmp/selection.csv
+    xvfb-run /opt/mca-selector/bin/java -D/opt/mca-selector/bin -Xmx6g -jar /opt/mca-selector/lib/mca-selector.jar --mode image --world /opt/data/world --selection /tmp/selection.csv --output $imageFile
+    rm /tmp/selection.csv
     aws s3 cp /tmp/world.png $ALBUMBUCKETS3URI
     aws s3 cp /tmp/world.png $FILEBUCKETS3URI/album/
+    rm /tmp/world.png
     echo Generating png of world complete >&2
 }
 
