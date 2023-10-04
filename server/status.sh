e5535c96a4e8c398ee5ee872561a7049d9601795
Diane Marigold <golbourn@gmail.com>
2023-10-04 22:01:57 +0100

    tweak server status calculation

--- server/status.sh
+++ server/status.sh
@@ -7,53 +7,50 @@ get_player_count() {
     echo ${tmp[4]}
 }
 
-ready() {
-    echo waiting
-    while true; do
-        nc -w 60s -z 127.0.0.1 25565 < /dev/null
-        if (( $? == 0 )); then
-            echo ready >&2
-            INSTANCE_ID=$(ec2-metadata --instance-id | cut -d " " -f 2)
-            aws lambda invoke --function-name $STARTCONTINUELAMBDA --payload "{\"detail\":{\"EC2InstanceId\":\"$INSTANCE_ID\"}}" --cli-binary-format raw-in-base64-out /dev/null
-            break
-        fi
-    done
-}
+REGEX='^[0-9]+$'
 
 status() {
     SECONDS=0
     DONE=false
+    READY=false
     PLAYERS=0
     while true; do
         COUNT=$(get_player_count)
-        COUNT=$(($COUNT+0))
-        if (( $COUNT > 0 )); then
-            SECONDS=0
-            if (( $COUNT != $PLAYERS )); then
-                PLAYERS=$COUNT
+        if [[ $COUNT =~ $REGEX ]]; then
+            if (( !$READY )); then
+                READY=true
+                echo ready >&2
                 echo $COUNT
+                INSTANCE_ID=$(ec2-metadata --instance-id | cut -d " " -f 2)
+                aws lambda invoke --function-name $STARTCONTINUELAMBDA --payload "{\"detail\":{\"EC2InstanceId\":\"$INSTANCE_ID\"}}" --cli-binary-format raw-in-base64-out /dev/null
             fi
-            if (( $DONE )); then
-                DONE=false
-                echo active >&2
-                aws lambda invoke --function-name $STARTSTOPLAMBDA --payload "{\"start\":true,\"referrer\":\"server\"}" --cli-binary-format raw-in-base64-out /dev/null
-            fi
-        elif (( $SECONDS > 3600 )); then
-            if (( !$DONE )); then
-                DONE=true
-                echo idle >&2
-                aws lambda invoke --function-name $STARTSTOPLAMBDA --payload "{\"start\":false,\"referrer\":\"server\"}" --cli-binary-format raw-in-base64-out /dev/null
-            fi
-        else
-            if (( $COUNT != $PLAYERS )); then
-                PLAYERS=$COUNT
-                echo $COUNT
-                echo quiet >&2
+            if (( $COUNT > 0 )); then
+                SECONDS=0
+                if (( $COUNT != $PLAYERS )); then
+                    PLAYERS=$COUNT
+                    echo $COUNT
+                fi
+                if (( $DONE )); then
+                    DONE=false
+                    echo active >&2
+                    aws lambda invoke --function-name $STARTSTOPLAMBDA --payload "{\"start\":true,\"referrer\":\"server\"}" --cli-binary-format raw-in-base64-out /dev/null
+                fi
+            elif (( $SECONDS > 3600 )); then
+                if (( !$DONE )); then
+                    DONE=true
+                    echo idle >&2
+                    aws lambda invoke --function-name $STARTSTOPLAMBDA --payload "{\"start\":false,\"referrer\":\"server\"}" --cli-binary-format raw-in-base64-out /dev/null
+                fi
+            else
+                if (( $COUNT != $PLAYERS )); then
+                    PLAYERS=$COUNT
+                    echo $COUNT
+                    echo quiet >&2
+                fi
             fi
         fi
         sleep 10
     done
 }
 
-ready
 status
