0c43ea27893044541535e66a77f6b9378a7fe989
Diane Marigold <golbourn@gmail.com>
2023-10-04 22:34:25 +0100

    no more force stop

--- server/force-stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /opt/.env
-aws lambda invoke --function-name $STARTSTOPLAMBDA --payload "{\"start\":false,\"referrer\":\"server\"}" --cli-binary-format raw-in-base64-out /dev/null
-aws lambda invoke --function-name $STARTSTOPLAMBDA --payload "{\"start\":false,\"referrer\":\"scheduler\"}" --cli-binary-format raw-in-base64-out /dev/null
