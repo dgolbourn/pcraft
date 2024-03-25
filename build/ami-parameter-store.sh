eb86218973d413fd70c27e8146b2ec6cb2734afb
Diane Marigold <golbourn@gmail.com>
2024-03-25 00:01:27 +0000

    Update ami-parameter-store.sh

--- build/ami-parameter-store.sh
+++ build/ami-parameter-store.sh
@@ -1,8 +1,8 @@
-  #!/bin/bash -xe
-  AMI_ID=$(jq -r '.builds[-1].artifact_id' packer-manifest.json | cut -d ":" -f2)
-  echo $AMI_ID >&2
-  aws ssm put-parameter \
-    --name "/percycraft/ami-latest/base-x86_64" \
-    --type "String" \
-    --value "$AMI_ID" \
-    --overwrite
+#!/bin/bash -xe
+AMI_ID=$(jq -r '.builds[-1].artifact_id' packer-manifest.json | cut -d ":" -f2)
+echo $AMI_ID >&2
+aws ssm put-parameter \
+  --name "/percycraft/ami-latest/base-x86_64" \
+  --type "String" \
+  --value "$AMI_ID" \
+  --overwrite
