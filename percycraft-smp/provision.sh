b60e6a28ab693189ba8c537adf2695c35d6ec387
Diane Marigold <golbourn@gmail.com>
2024-03-27 00:08:15 +0000

    Update provision.sh

--- percycraft-smp/provision.sh
+++ percycraft-smp/provision.sh
@@ -7,7 +7,7 @@ git describe --tags --long --always > /opt/data/percycraft.version
 /usr/local/bin/docker-compose -f /opt/percycraft/percycraft-smp/install-minecraft.yml up
 rm -rf /opt/data/.modrinth-manifest.json
 chmod +x /opt/percycraft/mods/enhancedgroups/provision.sh
-chmod +x /opt/percycraft/mods/friendlyfire/provision.sh
+chmod +x /opt/percycraft/mods/friendly-fire/provision.sh
 /opt/percycraft/mods/enhancedgroups/provision.sh
 /opt/percycraft/mods/friendlyfire/provision.sh
 chown -R 1000:1000 /opt/data
