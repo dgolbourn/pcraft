b49825be1c729e3a19f64abd3ebd187deca1e1ba
Diane Marigold <golbourn@gmail.com>
2024-03-27 00:41:52 +0000

    Update provision.sh

--- percycraft-smp/provision.sh
+++ percycraft-smp/provision.sh
@@ -9,7 +9,7 @@ rm -rf /opt/data/.modrinth-manifest.json
 chmod +x /opt/percycraft/mods/enhancedgroups/provision.sh
 chmod +x /opt/percycraft/mods/friendly-fire/provision.sh
 /opt/percycraft/mods/enhancedgroups/provision.sh
-/opt/percycraft/mods/friendlyfire/provision.sh
+/opt/percycraft/mods/friendly-fire/provision.sh
 chown -R 1000:1000 /opt/data
 
 echo Install server complete >&2
