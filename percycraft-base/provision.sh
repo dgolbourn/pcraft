1c29d8148cfd146c648f439b1255139105ced578
Diane Marigold <golbourn@gmail.com>
2024-03-30 21:35:06 +0000

    fix provision.sh, update docker-compose

--- percycraft-base/provision.sh
+++ percycraft-base/provision.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 echo Provision server started >&2
 
-git() {
+provision_git() {
     dnf update -y
     dnf -y install git < /dev/null
 }
 
-docker() {
+provision_docker() {
     echo Install docker started >&2
-    dnf install -y docker < /dev/null
-    curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose < /dev/null
-    chmod +x /usr/local/bin/docker-compose
+    sudo dnf -y install dnf-plugins-core < /dev/null
+    sudo dnf -y config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo < /dev/null
+    sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin < /dev/null
     systemctl enable docker.service
     systemctl start docker.service
     echo Install docker complete >&2
 }
 
-mcrcon() {
+provision_mcrcon() {
     echo Install mcrcon started >&2
-    curl -fsSL -o mcrcon.tar.gz https://github.com/Tiiffi/mcrcon/releases/download/v0.7.2/mcrcon-0.7.2-linux-x86-64.tar.gz < /dev/null
+    curl -fL -o mcrcon.tar.gz https://github.com/Tiiffi/mcrcon/releases/download/v0.7.2/mcrcon-0.7.2-linux-x86-64.tar.gz < /dev/null
     tar -xvzf mcrcon.tar.gz
     mv mcrcon /usr/local/bin/
     rm mcrcon.tar.gz
@@ -26,11 +26,11 @@ mcrcon() {
     echo Install mcrcon complete >&2
 }
 
-mcaselector() {
+provision_mcaselector() {
     echo Install mcaselector started >&2
     dnf install -y gtk3-devel < /dev/null
     dnf install -y xorg-x11-server-Xvfb< /dev/null
-    curl -o javafx.tar.gz https://cdn.azul.com/zulu/bin/zulu17.30.15-ca-fx-jre17.0.1-linux_x64.tar.gz < /dev/null
+    curl -fL -o javafx.tar.gz https://cdn.azul.com/zulu/bin/zulu17.30.15-ca-fx-jre17.0.1-linux_x64.tar.gz < /dev/null
     tar -xvzf javafx.tar.gz
     mv zulu* /opt/mca-selector
     rm javafx.tar.gz
@@ -39,27 +39,27 @@ mcaselector() {
     echo Install mcaselector complete >&2
 }
 
-statusbase() {
+provision_status() {
     echo Install status started >&2
     dnf install -y nc < /dev/null
     echo Install status complete >&2
 }
 
-percycraftbase() {
+provision_percycraft() {
     echo Install percycraft started >&2
     dnf install -y tree < /dev/null
-    docker pull amake/innosetup 
-    docker tag amake/innosetup amake/innosetup:base
-    docker pull itzg/minecraft-server
-    docker tag itzg/minecraft-server itzg/minecraft-server:base
+    docker image pull amake/innosetup 
+    docker image tag amake/innosetup amake/innosetup:base
+    docker image pull itzg/minecraft-server
+    docker image tag itzg/minecraft-server itzg/minecraft-server:base
     echo Install percycraft complete >&2
 }
 
-git
-docker
-percycraftbase
-mcrcon
-mcaselector
-statusbase
+provision_git
+provision_docker
+provision_mcrcon
+provision_mcaselector
+provision_status
+provision_percycraft
 
 echo Provision server complete >&2
--- percycraft-smp/provision.sh
+++ percycraft-smp/provision.sh
@@ -4,7 +4,7 @@ echo Install server started >&2
 cd /opt/percycraft
 mkdir -p /opt/data
 git describe --tags --long --always > /opt/data/percycraft.version
-/usr/local/bin/docker-compose -f /opt/percycraft/percycraft-smp/install-minecraft.yml up
+docker compose -f /opt/percycraft/percycraft-smp/install-minecraft.yml up
 rm -rf /opt/data/.modrinth-manifest.json
 chmod +x /opt/percycraft/mods/enhancedgroups/provision.sh
 chmod +x /opt/percycraft/mods/friendly-fire/provision.sh
