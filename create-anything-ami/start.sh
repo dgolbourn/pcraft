c9b54198be22e5182d16acfa2eb5fb0576b77859
Diane Marigold <golbourn@gmail.com>
2024-04-05 00:07:29 +0100

    create anything

---
+++ create-anything-ami/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+echo User Data started >&2
+source /opt/.env
+
+echo OPS=$WHITELIST > /opt/.env
+
+echo User Data complete >&2
--- percycraft-smp-ami/provision.sh
+++ percycraft-smp-ami/provision.sh
@@ -21,7 +21,6 @@ provision_minecraft() {
     echo Provision minecraft started >&2
     mkdir -p /opt/data
     docker compose -f /tmp/percycraft/percycraft-smp-ami/provision-minecraft.yml up --exit-code-from provision-minecraft
-    rm -rf /opt/data/.modrinth-manifest.json
     cd /opt/data
     JAR=$(ls -t *.jar | head -1)
     echo "CUSTOM_SERVER=/data/${JAR}" >> /opt/.env
